994b142b92a73213262b42b4ea01728b10e8cfbd
[eddmoura] <edd.alca@gmail.com>
2017-06-24 20:50:24 -0400

    Functional

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,10 +6,10 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
 
-  // Your username
+ 
   user: "root",
 
-  // Your password
+  
   password: "",
   database: "bamazon"
 });
@@ -19,10 +19,10 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
   displaytable();
 });
-
+// npm "cli-table" is used here for a better view of the table in the Terminal.
 var displaytable = function() {
 connection.query("SELECT * FROM products", function(err, res) {
-   if (err) throw err;
+      if (err) throw err;
   
    var table = new Table({
     head:["item_id", "product_name", "department_name", "price", "stock_quantity"],
@@ -34,7 +34,84 @@ connection.query("SELECT * FROM products", function(err, res) {
       table.push(bamazonProducts);
     }
     console.log(table.toString());
+    shoppingCart();
 
+  });
+};
+
+var shoppingCart = function(){
+      inquirer.prompt([{
+       //e
+        name: "Item_id",
+        type: "input",
+        message: "Please, enter the ID number of the item you would like to purchase."
+
+      },
+      {
+      name:"Quantity",
+      type: "input",
+      message: "How many would you like to buy?"
+
+  }]).then(function (answers) {
+
+    var select = "SELECT * FROM products WHERE item_id=" + answers.Item_id;
+     
+     // if(select != answers.Item_id){
+     //    console.log("Sorry ID not valid!");
+     //    displaytable();
+     //   }
+    
+    connection.query(select, function(err, res){
+       
+      if (err) throw err;
+      
+      //if quantity selected by user is greater than the available in stock will show the the warning and the table will be display for another try.
+      if (answers.Quantity > res[0].stock_quantity){ 
+      console.log("Sorry, amount request not available.")
+      displaytable();} 
+     //update the stock quantity 
+      else{
+          connection.query("UPDATE products SET? WHERE?", [{stock_quantity: res[0].stock_quantity - answers.Quantity},
+          
+          {item_id: answers.Item_id}],
+
+          function(err, res){
+            
+            if(err) throw err;
+
+          });
+        // calculate the price of the specify ID times the qunatity selected by the user.
+        var total = res[0].price * answers.Quantity;
+        console.log("Your total is: $ " + total);
+
+        // Ask if customer would like to keep shopping
+         inquirer.prompt({
+
+         name: "keepshoppimg",
+         type: "confirm",
+         message: "Would you like to keep shopping?"
+
+        }).then(function(answer){
+           //Display table if prompt Y
+           if(answer.keepshoppimg === true){
+           displaytable();} 
+           // end the connection if prompt N
+           else{
+           console.log("Thanks for shopping with us!");
+           connection.end();
+         };
+
+        });
+
+      };
+
+    });
+         
   });
 
 };
+
+
+
+
+
